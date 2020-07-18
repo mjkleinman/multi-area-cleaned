981767c81791239cd383fe95414b44ca9ac6b8bd
Michael Kleinman <kleinman_m@hotmail.com>
2020-07-18 11:02:52 -0700

    set default to 8 seeds, single RNN dpca variance

--- cfg_mk.py
+++ cfg_mk.py
@@ -2,7 +2,7 @@ cfg_mk = {
     'diff_area_noise': False,  # Add selective recurrent noise to areas
     'area_noise': 3,
 
-    'linear_mut_info': False, #True if you want the linear decoder
+    'linear_mut_info': False,  # True if you want the linear decoder
 
     'path': '/Users/michael/Documents/GitHub/multi-area-cleaned/',
     'rnn_datapath': '/Users/michael/Documents/GitHub/multi-area-cleaned/saved_rnns_server_apr/data/',
@@ -11,7 +11,7 @@ cfg_mk = {
     'rnn_areas': 3,  # only matters for analysis
     'modelpath': '2020-04-10_cb_simple_3areas',  # _nodale_ff=0p1',  # make sure this matches num_rnn_areas
     'num_units': 300,
-    'num_seeds': 1,  # -1 if did not specify
+    'num_seeds': 8,  # -1 if did not specify
 
     'gamma_rec': True,  # True if recurrent distribution for Crec is gamma
     'random_mask_wrec': False,  # default is False
--- sims/get_dpca_vals.sh
+++ sims/get_dpca_vals.sh
@@ -41,24 +41,23 @@ python analyze_color_var.py --modelpath '2020-04-10_cb_simple_3areas' --suffix '
 python analyze_color_var.py --modelpath '2020-04-10_cb_simple_3areas' --suffix '_lr=5e-4'
 python analyze_color_var.py --modelpath '2020-04-10_cb_simple_3areas' --suffix '_lr=7.5e-4'
 
+# single area
+python analyze_color_var.py --num_seeds 2 --suffix '_lambdaw=1e-1' --rnn_areas 1 --modelpath '2020-04-10_cb_simple_1area'
+python analyze_color_var.py --num_seeds 2 --suffix '_lambdaw=1e-2' --rnn_areas 1 --modelpath '2020-04-10_cb_simple_1area'
+python analyze_color_var.py --num_seeds 2 --suffix '_lambdar=1' --rnn_areas 1 --modelpath '2020-04-10_cb_simple_1area'
+python analyze_color_var.py --num_seeds 2 --suffix '_lambdar=1e-1' --rnn_areas 1 --modelpath '2020-04-10_cb_simple_1area'
+python analyze_color_var.py --num_seeds 2 --suffix '_lr=1e-4' --rnn_areas 1 --modelpath '2020-04-10_cb_simple_1area'
+python analyze_color_var.py --num_seeds 2 --suffix '_lr=1e-5' --rnn_areas 1 --modelpath '2020-04-10_cb_simple_1area'
+python analyze_color_var.py --num_seeds 2 --suffix '_lr=2.5e-4' --rnn_areas 1 --modelpath '2020-04-10_cb_simple_1area'
+python analyze_color_var.py --num_seeds 2 --suffix '_lr=5e-4' --rnn_areas 1 --modelpath '2020-04-10_cb_simple_1area'
+python analyze_color_var.py --num_seeds 2 --suffix '_lr=7.5e-4' --rnn_areas 1 --modelpath '2020-04-10_cb_simple_1area'
+
+
 ### STOP HERE!
 
 
 # python analyze_color_var.py --rnn_datapath '/Users/michael/Desktop/tibi_backup/tibi/saved_rnns/three_rnns/' --modelpath '2018-08-29_cb_3areas_ff0p2_fb0p05'
 # python analyze_color_var.py --rnn_datapath '/Users/michael/Desktop/tibi_backup/tibi/saved_rnns/three_rnns/' --modelpath '2018-08-29_cb_3areas_ff0p3_fb0p05'
-
-
-
-## RUN THESE
 # python analyze_color_var.py --rnn_datapath '/Users/michael/Desktop/tibi_backup/tibi/saved_rnns/three_rnns/' --modelpath '2018-08-29_cb_3areas_ff0p1_fb0p0'
 # python analyze_color_var.py --rnn_datapath '/Users/michael/Desktop/tibi_backup/tibi/saved_rnns/three_rnns/' --modelpath '2018-08-29_cb_3areas_ff0p1_fb0p1'
 
-# python analyze_color_var.py --num_seeds 2 --suffix '_lambdaw=1e-1' --rnn_areas 1 --modelpath '2020-04-10_cb_simple_1area'
-# python analyze_color_var.py --num_seeds 2 --suffix '_lambdaw=1e-2' --rnn_areas 1 --modelpath '2020-04-10_cb_simple_1area'
-# python analyze_color_var.py --num_seeds 2 --suffix '_lambdar=1' --rnn_areas 1 --modelpath '2020-04-10_cb_simple_1area'
-# python analyze_color_var.py --num_seeds 2 --suffix '_lambdar=1e-1' --rnn_areas 1 --modelpath '2020-04-10_cb_simple_1area'
-# python analyze_color_var.py --num_seeds 2 --suffix '_lr=1e-4' --rnn_areas 1 --modelpath '2020-04-10_cb_simple_1area'
-# python analyze_color_var.py --num_seeds 2 --suffix '_lr=1e-5' --rnn_areas 1 --modelpath '2020-04-10_cb_simple_1area'
-# python analyze_color_var.py --num_seeds 2 --suffix '_lr=2.5e-4' --rnn_areas 1 --modelpath '2020-04-10_cb_simple_1area'
-# python analyze_color_var.py --num_seeds 2 --suffix '_lr=5e-4' --rnn_areas 1 --modelpath '2020-04-10_cb_simple_1area'
-# python analyze_color_var.py --num_seeds 2 --suffix '_lr=7.5e-4' --rnn_areas 1 --modelpath '2020-04-10_cb_simple_1area'
